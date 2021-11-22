58f705c3a1d2bfbf2ad0a58e4eb53107e6ebffb9
r3x6 <rexchin94@gmail.com>
2021-11-22 13:35:58 +0800

    created enemy image and positioning

--- td_files/tdCode.js
+++ td_files/tdCode.js
@@ -11,7 +11,11 @@ function generateGrounds(len) {
   if (len % 2 !== 0) {
     for (let i = 0; i < len ** 2; i++) {
       if (i < len * Math.ceil(len / 2) && i >= len * Math.floor(len / 2)) {
-        makeSquare("valley");
+        if (i === len * Math.floor(len / 2)) {
+          makeSquare("endPt");
+        } else if (i === len * Math.ceil(len / 2) - 1) {
+          makeSquare("startPt");
+        } else makeSquare("valley");
       } else {
         makeSquare();
       }
@@ -35,3 +39,25 @@ const unlit = (e) => {
 };
 document.getElementById("grounds").addEventListener("mouseover", lit);
 document.getElementById("grounds").addEventListener("mouseout", unlit);
+
+const rect = document.getElementById("startPt").getBoundingClientRect();
+
+const spawnEnemy = () => {
+  const enemy = document.createElement("div");
+  enemy.className = "enemy";
+  const enemyHp = document.createElement("div");
+  enemyHp.className = "enemyHp";
+  const enemyLostHp = document.createElement("div");
+  enemyLostHp.className = "enemyLostHp";
+  const enemySprite = document.createElement("div");
+  enemySprite.className = "enemySprite";
+  enemy.append(enemyHp);
+  enemy.append(enemyLostHp);
+  enemy.append(enemySprite);
+  enemy.className = "mob";
+  enemy.style.top = `${rect.y + 12}px`;
+  enemy.style.left = `${rect.x + 15}px`;
+
+  document.body.append(enemy);
+};
+spawnEnemy();
